4487d9d0be7231a59db2dd9ce2124b35a47af752
thurunu <156066585+thurunu711@users.noreply.github.com>
2024-12-04 14:26:15 +0530

    Create ParkingSlots.kt

---
+++ app/src/main/java/com/example/mmthapp/ParkingSlots.kt
@@ -0,0 +1,10 @@
+package com.example.mmthapp
+
+data class ParkingSlots(
+    val UserID: String,
+    val Date: String,
+    val Time: String,
+    val VehicleID: String,
+    val SlotID: String
+
+)
